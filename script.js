96b8c15cf18e2821b355089e06c24225ad085f7b
Batman <batman@batbookpro.home>
2025-01-01 04:54:36 -0500

    added favicon, bottom positioning, js for selector

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function changePageTitle() {
-    firstPageTitle = 'Custom Address Lables';
-    secondPageTitle = 'Order Yours Now!'
-    document.querySelector('title').textContent
-        = firstPageTitle;
+// Revolving text in Title tag
+let titleText = ['My Home Stickers', 'Custom Labels', 'Order Now!'];
+
+let index = 0;
+const mainTitle = document.querySelector('title');
+
+function change() {
+   mainTitle.innerHTML = titleText[index];
+   index > 1 ? index = 0 : index++;
+}
+
+window.onload = function () {
+    setInterval(change, 5000);
+};
+
+
+// Image selector for different colors
+let greyBtn = document.getElementById("greyS");
+let greenBtn = document.getElementById("greenS");
+let blueBtn = document.getElementById("blueS");
+
+let preImage = document.getElementById("previewImage");
+
+greyBtn.onclick = function(){
+    preImage.style.backgroundImage="url(images/previewG.png)";
+}
+
+greenBtn.onclick = function(){
+    preImage.style.backgroundImage="url(images/previewGr.png)";
+}
+
+blueBtn.onclick = function(){
+    preImage.style.backgroundImage="url(images/previewB.png)";
 }
